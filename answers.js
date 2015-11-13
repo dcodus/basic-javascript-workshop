b715db82e8a1e93981b01eba1233ae4c57b852eb
Codrin Diaconu <diaconu.codrin@gmail.com>
2015-11-13 18:37:15 +0000

    Added the firstCharacter function

--- answers.js
+++ answers.js
@@ -1,1 +1,10 @@
 // This is a one line comment
+
+//Created a function that takes a string and return the first character
+
+function firstCharacter(string){
+    return string.charAt(0);
+}
+firstCharacter('hello');
+
+
